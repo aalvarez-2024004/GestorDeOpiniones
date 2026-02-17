5c93ee121d6dea9685fb3dd0a0a7e8690e0fee87
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2026-02-17 16:31:29 -0600

    Se agrego el modelado de usuarios, el controlador, el servicio y las rutas

---
+++ src/users/users.routes.js
@@ -0,0 +1,22 @@
+import { Router } from 'express'
+import { validateJWT } from '../../middlewares/validate-jwt.js'
+import { validateRole } from '../../middlewares/validate-role.js'
+import { updateMyPassword } from './user.controller.js'
+
+const router = Router()
+
+router.post(
+  '/create-admin-restaurant',
+  validateJWT,
+  validateRole('ADMIN_GENERAL'),
+  createAdminRest
+)
+
+router.patch(
+  '/change-password',
+  validateJWT,
+  validateRole('ADMIN_GENERAL', 'USUARIO'),
+  updateMyPassword
+)
+
+export default router
